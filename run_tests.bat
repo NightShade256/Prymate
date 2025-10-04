9070dba176a9cf21632fee89f17a0940a105e827
Anish Jewalikar <anishjewalikar@gmail.com>
2025-10-05 03:08:20 +0530

    tests: remove github actions and test run scripts

--- run_tests.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-echo Running Monkey Language Tests
-python -m unittest tests.lexer_test tests.ast_test tests.parser_test tests.evaluator_test tests.objects_test
--- run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo Running Monkey Language Tests
-
-# Run Prymate Tests, for Linux Distros and Mac OS only.
-python3 -m unittest tests.lexer_test tests.ast_test tests.parser_test tests.evaluator_test tests.objects_test
