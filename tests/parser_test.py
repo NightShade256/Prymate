8dcb4c304c9d08fa8e098acabe763a0ff65de01d
Anish Jewalikar <anishjewalikar@gmail.com>
2025-10-08 23:43:22 +0530

    parameterize parser tests with multiple cases

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -25,50 +25,52 @@ def infix_expression_helper(
     assert expression.right == right
 
 
-def test_let_statement():
-    tests = [
+@pytest.mark.parametrize(
+    "source, identifier, expression",
+    [
         ("let x = 5;", "x", ast.IntegerLiteral(5)),
         ("let y = true;", "y", ast.BooleanLiteral(True)),
         ("let foobar = y;", "foobar", ast.Identifier("y")),
         ("let foobar = 45.5;", "foobar", ast.FloatLiteral(45.5)),
         ('let asdf = "asdf";', "asdf", ast.StringLiteral("asdf")),
-    ]
+    ],
+)
+def test_let_statement(source: str, identifier: str, expression: ast.Expression):
+    scanner = Scanner(source)
+    parser = Parser(scanner)
 
-    for test_case in tests:
-        scanner = Scanner(test_case[0])
-        parser = Parser(scanner)
+    tree = parser.parse_program()
 
-        tree = parser.parse_program()
+    print(tree)
 
-        print(tree)
+    statement = tree.statements[0]
+    check_parser_errors(parser)
 
-        statement = tree.statements[0]
-        check_parser_errors(parser)
-
-        assert len(tree.statements) == 1
-        assert isinstance(statement, ast.LetStatement)
-        assert statement.lvalue.name == test_case[1]
-        assert statement.rvalue == test_case[2]
+    assert len(tree.statements) == 1
+    assert isinstance(statement, ast.LetStatement)
+    assert statement.lvalue.name == identifier
+    assert statement.rvalue == expression
 
 
-def test_return_statement():
-    tests = [
+@pytest.mark.parametrize(
+    "source, expression",
+    [
         ("return 5;", ast.IntegerLiteral(5)),
         ("return true;", ast.BooleanLiteral(True)),
         ("return foobar;", ast.Identifier("foobar")),
-    ]
+    ],
+)
+def test_return_statement(source: str, expression: ast.Expression):
+    scanner = Scanner(source)
+    parser = Parser(scanner)
 
-    for test_case in tests:
-        scanner = Scanner(test_case[0])
-        parser = Parser(scanner)
+    tree = parser.parse_program()
+    statement = tree.statements[0]
+    check_parser_errors(parser)
 
-        tree = parser.parse_program()
-        statement = tree.statements[0]
-        check_parser_errors(parser)
-
-        assert len(tree.statements) == 1
-        assert isinstance(statement, ast.ReturnStatement)
-        assert statement.value == test_case[1]
+    assert len(tree.statements) == 1
+    assert isinstance(statement, ast.ReturnStatement)
+    assert statement.value == expression
 
 
 def test_identifier_statement():
@@ -113,31 +115,33 @@ def test_string_literal_statement():
     assert statement.expression.value == "hello world"
 
 
-def test_prefix_expression():
-    tests = [
+@pytest.mark.parametrize(
+    "source, operator, expression",
+    [
         ("!5;", "!", ast.IntegerLiteral(5)),
         ("-15;", "-", ast.IntegerLiteral(15)),
         ("!true;", "!", ast.BooleanLiteral(True)),
         ("!false;", "!", ast.BooleanLiteral(False)),
-    ]
+    ],
+)
+def test_prefix_expression(source: str, operator: str, expression: ast.Expression):
+    scanner = Scanner(source)
+    parser = Parser(scanner)
 
-    for test_case in tests:
-        scanner = Scanner(test_case[0])
-        parser = Parser(scanner)
+    tree = parser.parse_program()
+    statement = tree.statements[0]
+    check_parser_errors(parser)
 
-        tree = parser.parse_program()
-        statement = tree.statements[0]
-        check_parser_errors(parser)
-
-        assert len(tree.statements) == 1
-        assert isinstance(statement, ast.ExpressionStatement)
-        assert isinstance(statement.expression, ast.PrefixExpression)
-        assert statement.expression.operator == test_case[1]
-        assert statement.expression.operand == test_case[2]
+    assert len(tree.statements) == 1
+    assert isinstance(statement, ast.ExpressionStatement)
+    assert isinstance(statement.expression, ast.PrefixExpression)
+    assert statement.expression.operator == operator
+    assert statement.expression.operand == expression
 
 
-def test_infix_expression():
-    tests = [
+@pytest.mark.parametrize(
+    "source, left, operator, right",
+    [
         ("5 + 5;", ast.IntegerLiteral(5), "+", ast.IntegerLiteral(5)),
         ("5 - 5;", ast.IntegerLiteral(5), "-", ast.IntegerLiteral(5)),
         ("5 * 5;", ast.IntegerLiteral(5), "*", ast.IntegerLiteral(5)),
@@ -151,22 +155,21 @@ def test_infix_expression():
         ("true == true;", ast.BooleanLiteral(True), "==", ast.BooleanLiteral(True)),
         ("true != false;", ast.BooleanLiteral(True), "!=", ast.BooleanLiteral(False)),
         ("false == false;", ast.BooleanLiteral(False), "==", ast.BooleanLiteral(False)),
-    ]
+    ],
+)
+def test_infix_expression(
+    source: str, left: ast.Expression, operator: str, right: ast.Expression
+):
+    scanner = Scanner(source)
+    parser = Parser(scanner)
 
-    for test_case in tests:
-        scanner = Scanner(test_case[0])
-        parser = Parser(scanner)
+    tree = parser.parse_program()
+    statement = tree.statements[0]
+    check_parser_errors(parser)
 
-        tree = parser.parse_program()
-        statement = tree.statements[0]
-        check_parser_errors(parser)
-
-        assert len(tree.statements) == 1
-        assert isinstance(statement, ast.ExpressionStatement)
-
-        infix_expression_helper(
-            statement.expression, test_case[1], test_case[2], test_case[3]
-        )
+    assert len(tree.statements) == 1
+    assert isinstance(statement, ast.ExpressionStatement)
+    infix_expression_helper(statement.expression, left, operator, right)
 
 
 def test_if_expression():
@@ -223,28 +226,29 @@ def test_if_else_expression():
     assert alternative.statements[0].expression == ast.Identifier("y")
 
 
-def test_function_parameter():
-    tests: list[tuple[str, list[ast.Expression]]] = [
+@pytest.mark.parametrize(
+    "source, parameters",
+    [
         ("fn() {};", []),
         ("fn(x) {};", [ast.Identifier("x")]),
         (
             "fn(x, y, z) {};",
             [ast.Identifier("x"), ast.Identifier("y"), ast.Identifier("z")],
         ),
-    ]
+    ],
+)
+def test_function_parameter(source: str, parameters: list[ast.Expression]):
+    scanner = Scanner(source)
+    parser = Parser(scanner)
 
-    for test_case in tests:
-        scanner = Scanner(test_case[0])
-        parser = Parser(scanner)
+    tree = parser.parse_program()
+    statement = tree.statements[0]
+    check_parser_errors(parser)
 
-        tree = parser.parse_program()
-        statement = tree.statements[0]
-        check_parser_errors(parser)
-
-        assert len(tree.statements) == 1
-        assert isinstance(statement, ast.ExpressionStatement)
-        assert isinstance(statement.expression, ast.FunctionLiteral)
-        assert statement.expression.parameters == test_case[1]
+    assert len(tree.statements) == 1
+    assert isinstance(statement, ast.ExpressionStatement)
+    assert isinstance(statement.expression, ast.FunctionLiteral)
+    assert statement.expression.parameters == parameters
 
 
 def test_function_literal():
